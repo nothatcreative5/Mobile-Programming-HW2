1d84021c1c6e0934e093e300266ab1f341abb6b5
Sadegh <mohammadsadeghmajidiyazdi@hotmail.com>
2022-04-30 01:39:27 +0430

    implementig api calls v1

---
+++ app/src/main/java/edu/sharif/weather/controller/WeatherController.java
@@ -0,0 +1,68 @@
+package edu.sharif.weather.controller;
+
+import android.util.JsonReader;
+
+//import com.google.gson.JsonObject;
+
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Objects;
+
+import org.json.*;
+
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class WeatherController {
+    private final OkHttpClient client;
+
+    public WeatherController() {
+        client = new OkHttpClient();
+    }
+
+    public String getWeatherByGeoLocation(String lat, String lon) throws IOException {
+        HttpUrl.Builder urlBuilder = HttpUrl.parse("https://api.openweathermap.org/data/2.5/onecall").newBuilder();
+        urlBuilder.addQueryParameter("lat", lat);
+        urlBuilder.addQueryParameter("lon", lon);
+//        urlBuilder.addQueryParameter("exclude", "hourly");
+        urlBuilder.addQueryParameter("appid", "da791b2b3446f7ecaaf66562a11f07cd");
+        String url = urlBuilder.build().toString();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
+
+    public  HashMap<String, String> getGeoLocation(String locationName) throws IOException {
+        HttpUrl.Builder urlBuilder = HttpUrl.parse("https://api.mapbox.com/geocoding/v5/mapbox.places/" + locationName +".json").newBuilder();
+        urlBuilder.addQueryParameter("access_token", "pk.eyJ1Ijoic2FkZWdoLW1hamlkaSIsImEiOiJjbDJrdjE3Y3EwZ3VxM2pvOGgxNWNmazhpIn0.TKYyVlsskmF5F5HFkwK5zg");
+        String url = urlBuilder.build().toString();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        Response response = client.newCall(request).execute();
+        String body = Objects.requireNonNull(response.body()).string();
+        try {
+            JSONObject obj = new JSONObject(body);
+            JSONArray geoLocArr = obj.getJSONArray("features").getJSONObject(0).getJSONArray("center");
+            HashMap<String, String> geoLoc = new HashMap<>();
+            geoLoc.put("lat", String.valueOf(geoLocArr.getDouble(1)));
+            geoLoc.put("lon", String.valueOf(geoLocArr.getDouble(0)));
+            return geoLoc;
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public String getWeatherByLocationName(String locationName) {
+
+        return "";
+    }
+}
--- app/src/test/java/edu/sharif/weather/ExampleUnitTest.java
+++ app/src/test/java/edu/sharif/weather/ExampleUnitTest.java
@@ -4,6 +4,11 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+import java.util.HashMap;
+
+import edu.sharif.weather.controller.WeatherController;
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -11,7 +16,29 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
+    public void weather_isCorrect() {
+        WeatherController wc = new WeatherController();
+        try {
+            String x = wc.getWeatherByGeoLocation("33.44", "-94.04");
+            System.out.println(x);
+        } catch (IOException e) {
+            System.out.println(1111);
+            e.printStackTrace();
+        }
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void location_isCorrect() {
+        WeatherController wc = new WeatherController();
+        try {
+            HashMap<String, String> x = wc.getGeoLocation("Los Angeles");
+            System.out.println(x.get("lon"));
+            System.out.println(x.get("lat"));
+        } catch (IOException e) {
+            System.out.println(1111);
+            e.printStackTrace();
+        }
         assertEquals(4, 2 + 2);
     }
 }
