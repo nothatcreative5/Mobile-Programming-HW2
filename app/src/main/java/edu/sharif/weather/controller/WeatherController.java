0391cff2c47f1d23ddfdfeda9f338c3bce41e1d7
Sadegh <mohammadsadeghmajidiyazdi@hotmail.com>
2022-04-30 15:53:32 +0430

    implement weather by location api

--- app/src/main/java/edu/sharif/weather/controller/WeatherController.java
+++ app/src/main/java/edu/sharif/weather/controller/WeatherController.java
@@ -20,21 +20,20 @@ public class WeatherController {
         client = new OkHttpClient();
     }
 
-    public JSONObject getWeatherByGeoLocation(String lat, String lon) throws IOException {
-        HttpUrl.Builder urlBuilder = HttpUrl.parse("https://api.openweathermap.org/data/2.5/onecall").newBuilder();
-        urlBuilder.addQueryParameter("lat", lat);
-        urlBuilder.addQueryParameter("lon", lon);
-        urlBuilder.addQueryParameter("exclude", "minutely,hourly,daily");
-        urlBuilder.addQueryParameter("units", "metric");
-        urlBuilder.addQueryParameter("appid", "da791b2b3446f7ecaaf66562a11f07cd");
-        String url = urlBuilder.build().toString();
-
-        Request request = new Request.Builder()
-                .url(url)
-                .build();
-        Response response = client.newCall(request).execute();
-        String body = Objects.requireNonNull(response.body()).string();
+    public JSONObject getWeatherByGeoLocation(String lat, String lon) {
         try {
+            HttpUrl.Builder urlBuilder = HttpUrl.parse("https://api.openweathermap.org/data/2.5/onecall").newBuilder();
+            urlBuilder.addQueryParameter("lat", lat);
+            urlBuilder.addQueryParameter("lon", lon);
+            urlBuilder.addQueryParameter("exclude", "minutely,hourly,daily");
+            urlBuilder.addQueryParameter("units", "metric");
+            urlBuilder.addQueryParameter("appid", "da791b2b3446f7ecaaf66562a11f07cd");
+            String url = urlBuilder.build().toString();
+
+            Request request = new Request.Builder().url(url).build();
+            Response response = client.newCall(request).execute();
+            String body = Objects.requireNonNull(response.body()).string();
+
             JSONObject obj = new JSONObject(body);
             JSONObject currentWeather = obj.getJSONObject("current");
             JSONObject output = new JSONObject();
@@ -47,37 +46,43 @@ public class WeatherController {
             weather.put("icon", currentWeather.getJSONArray("weather").getJSONObject(0).getString("icon"));
             output.put("weather", weather);
             return output;
-        } catch (JSONException e) {
+        } catch (IOException | JSONException e) {
             e.printStackTrace();
             return null;
         }
     }
 
-    public  HashMap<String, String> getGeoLocation(String locationName) throws IOException {
-        HttpUrl.Builder urlBuilder = HttpUrl.parse("https://api.mapbox.com/geocoding/v5/mapbox.places/" + locationName +".json").newBuilder();
-        urlBuilder.addQueryParameter("access_token", "pk.eyJ1Ijoic2FkZWdoLW1hamlkaSIsImEiOiJjbDJrdjE3Y3EwZ3VxM2pvOGgxNWNmazhpIn0.TKYyVlsskmF5F5HFkwK5zg");
-        String url = urlBuilder.build().toString();
-
-        Request request = new Request.Builder()
-                .url(url)
-                .build();
-        Response response = client.newCall(request).execute();
-        String body = Objects.requireNonNull(response.body()).string();
+    private HashMap<String, String> getGeoLocation(String locationName) {
         try {
+            HttpUrl.Builder urlBuilder = HttpUrl.parse("https://api.mapbox.com/geocoding/v5/mapbox.places/" + locationName + ".json").newBuilder();
+            urlBuilder.addQueryParameter("access_token", "pk.eyJ1Ijoic2FkZWdoLW1hamlkaSIsImEiOiJjbDJrdjE3Y3EwZ3VxM2pvOGgxNWNmazhpIn0.TKYyVlsskmF5F5HFkwK5zg");
+            String url = urlBuilder.build().toString();
+
+            Request request = new Request.Builder()
+                    .url(url)
+                    .build();
+            Response response = client.newCall(request).execute();
+            String body = Objects.requireNonNull(response.body()).string();
+
             JSONObject obj = new JSONObject(body);
             JSONArray geoLocArr = obj.getJSONArray("features").getJSONObject(0).getJSONArray("center");
             HashMap<String, String> geoLoc = new HashMap<>();
             geoLoc.put("lat", String.valueOf(geoLocArr.getDouble(1)));
             geoLoc.put("lon", String.valueOf(geoLocArr.getDouble(0)));
             return geoLoc;
-        } catch (JSONException e) {
+        } catch (IOException | JSONException e) {
             e.printStackTrace();
             return null;
         }
     }
 
-    public String getWeatherByLocationName(String locationName) {
-
-        return "";
+    public JSONObject getWeatherByLocationName(String locationName) {
+        HashMap<String, String> location = getGeoLocation(locationName);
+        try {
+            return getWeatherByGeoLocation(location.get("lat"), location.get("lon"));
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
--- app/src/test/java/edu/sharif/weather/ExampleUnitTest.java
+++ app/src/test/java/edu/sharif/weather/ExampleUnitTest.java
@@ -1,13 +1,9 @@
 package edu.sharif.weather;
 
+import static org.junit.Assert.assertEquals;
+
 import org.json.JSONObject;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
-
-import java.io.IOException;
-import java.util.Date;
-import java.util.HashMap;
 
 import edu.sharif.weather.controller.WeatherController;
 
@@ -20,27 +16,16 @@ public class ExampleUnitTest {
     @Test
     public void weather_isCorrect() {
         WeatherController wc = new WeatherController();
-        try {
-            JSONObject x = wc.getWeatherByGeoLocation("33.44", "-94.04");
-            System.out.println(x.toString());
-        } catch (IOException e) {
-            System.out.println(1111);
-            e.printStackTrace();
-        }
+        JSONObject x = wc.getWeatherByGeoLocation("33.44", "-94.04");
+        System.out.println(x.toString());
         assertEquals(4, 2 + 2);
     }
 
     @Test
     public void location_isCorrect() {
         WeatherController wc = new WeatherController();
-        try {
-            HashMap<String, String> x = wc.getGeoLocation("Los Angeles");
-            System.out.println(x.get("lon"));
-            System.out.println(x.get("lat"));
-        } catch (IOException e) {
-            System.out.println(1111);
-            e.printStackTrace();
-        }
+        JSONObject x = wc.getWeatherByLocationName("Tehran");
+        System.out.println(x.toString());
         assertEquals(4, 2 + 2);
     }
 }
