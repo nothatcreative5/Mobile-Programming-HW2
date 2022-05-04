8f3603a12cfcfef094712f8e5fd4a07f41679096
MohammadHejri <mohammadhejri.ce@gmail.com>
2022-05-04 20:16:20 +0430

    bottom navigation view + fragments

--- app/src/main/java/edu/sharif/weather/view/MainActivity.java
+++ app/src/main/java/edu/sharif/weather/view/MainActivity.java
@@ -1,8 +1,14 @@
 package edu.sharif.weather.view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import edu.sharif.weather.R;
 
@@ -12,5 +18,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            Fragment selectedFragment = new HomeFragment();
+            if (item.getItemId() == R.id.nav_settings)
+                selectedFragment = new SettingsFragment();
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+            return true;
+        });
     }
 }
