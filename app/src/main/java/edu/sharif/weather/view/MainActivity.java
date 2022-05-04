1a32a3779a42ac332f81d7b5903f3594faa63434
MohammadHejri <mohammadhejri.ce@gmail.com>
2022-05-04 18:02:40 +0430

    init bottom navigation activity

---
+++ app/src/main/java/edu/sharif/weather/view/MainActivity.java
@@ -0,0 +1,16 @@
+package edu.sharif.weather.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.sharif.weather.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
