ab5b3144593cbfee2e7b1f1fa2c6807d0527d13a
MohammadHejri <mohammadhejri.ce@gmail.com>
2022-05-16 12:19:51 +0430

    minor changes

--- app/src/main/java/edu/sharif/weather/view/MainActivity.java
+++ app/src/main/java/edu/sharif/weather/view/MainActivity.java
@@ -133,7 +133,7 @@ public class MainActivity extends AppCompatActivity {
             while ((line = lin.readLine()) != null) {
                 items.add(new CitySearchModel(line));
             }
-        } catch (IOException ignored) {
+        } catch (Exception ignored) {
         }
         return items;
     }
